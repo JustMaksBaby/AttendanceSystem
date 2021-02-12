fb546b198d52d4b8e52d26416d82efdfe151793a
Just Maks <just.maks.bro@gmail.com>
2021-02-12 18:09:22 +0200

    create login window

---
+++ AttendanceSystem/LoginWindow.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace AttendanceSystem
+{
+    partial class LoginWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.userTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.passwordTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.userLabel = new MetroFramework.Controls.MetroLabel();
+            this.passwordLabel = new MetroFramework.Controls.MetroLabel();
+            this.loginButton = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(70, 30);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(231, 170);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(70, 254);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(231, 23);
+            this.userTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(70, 330);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(231, 23);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.userLabel.Location = new System.Drawing.Point(70, 226);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(52, 25);
+            this.userLabel.TabIndex = 3;
+            this.userLabel.Text = "User:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.passwordLabel.Location = new System.Drawing.Point(70, 302);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(89, 25);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(186, 385);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(115, 42);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Log In";
+            // 
+            // LoginWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(385, 491);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LoginWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Log in ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private MetroFramework.Controls.MetroTextBox userTextBox;
+        private MetroFramework.Controls.MetroTextBox passwordTextBox;
+        private MetroFramework.Controls.MetroLabel userLabel;
+        private MetroFramework.Controls.MetroLabel passwordLabel;
+        private MetroFramework.Controls.MetroButton loginButton;
+    }
+}
