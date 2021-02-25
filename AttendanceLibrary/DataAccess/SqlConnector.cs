7eff3bbdf8fa0638dd1eec285a71e4ce4d090570
Just Maks <just.maks.bro@gmail.com>
2021-02-25 18:03:02 +0200

    added new methods and redid older to work with new models

--- AttendanceLibrary/DataAccess/SqlConnector.cs
+++ AttendanceLibrary/DataAccess/SqlConnector.cs
@@ -8,7 +8,7 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using AttendanceLibrary.Models;
 using System.Collections.Generic;
-
+using AttendanceLibrary.PasswordProcessing;
 namespace AttendanceLibrary.DataAccess
 {
     public class SqlConnector
@@ -17,57 +17,102 @@ namespace AttendanceLibrary.DataAccess
         private static readonly string _dbConnectionStr = ConfigurationManager.ConnectionStrings[_db].ConnectionString;
 
 
-        /// <summary>
-        /// Checks if the user with certain name already exists in the DB
-        /// </summary> 
-        public static bool UserExists(string userName)
+        public static bool UserExists(string login)
         {
             using (SqlConnection connection = new SqlConnection(_dbConnectionStr))
             {
                 DynamicParameters p = new DynamicParameters();
-                p.Add("@Name", userName);
+                p.Add("@Login", login);
 
-                int usersInDb = connection.ExecuteScalar<int>("dbo.spCheckUserByName", p, commandType: CommandType.StoredProcedure);
+                int usersInDb = connection.ExecuteScalar<int>("dbo.spCheckUserExist", p, commandType: CommandType.StoredProcedure);
 
                 return usersInDb > 0;
             }
         }
-        public static bool AddUser(User user)
+        public static void AddUser(ILoginInfo  loginInfo, Teacher teacher)
         {
-            if (!UserExists(user.Name)) // if user name is  available
+            using (SqlConnection connection = new SqlConnection(_dbConnectionStr))
             {
-                using (SqlConnection connection = new SqlConnection(_dbConnectionStr))
-                {
-                    DynamicParameters p = new DynamicParameters();
-                    p.Add("@Name", user.Name);
-                    p.Add("@Password", user.HashedPassword);
-                    p.Add("@Salt", user.Salt);
+                DynamicParameters p = new DynamicParameters();
 
-                    connection.Execute("dbo.spAddUser", p, commandType: CommandType.StoredProcedure);
-                }
-                return true;
+                p.Add("@Login", loginInfo.Login);
+                p.Add("@Password", loginInfo.CryptedPassword);
+                p.Add("@Salt", loginInfo.Salt);
+                p.Add("@UserId", AddTeacher(teacher)); 
+
+                connection.Execute("dbo.spAddLoginInfo", p, commandType: CommandType.StoredProcedure);
             }
-            else return false;
 
         }
-        /// <summary>
-        /// Gets a user by name
-        /// </summary>
-        public static User GetUserByName(string userName)
+        public static int AddTeacher(Teacher teacher)
         {
-            User output = null;
-            if (UserExists(userName))
+            //TODO check what will if there is no connection to database
+            int output = -1;  
+
+            using(SqlConnection connection = new SqlConnection(_dbConnectionStr))
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add("@Id", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                p.Add("@FirstName", teacher.FirstName);
+                p.Add("@LastName", teacher.LastName);
+                p.Add("@Patronymic",teacher.Patronymic);
+                p.Add("@SystemStatus", teacher.SystemStatus); 
+
+                connection.Execute("dbo.spAddTeacher", p, commandType: CommandType.StoredProcedure);
+
+                output = p.Get<int>("@Id"); 
+            }
+
+            return output;
+        }
+        public static bool TeacherExists(string firstName, string lastName)
+        {
+            int teachers = 0;  
+
+            using(SqlConnection connection = new SqlConnection(_dbConnectionStr))
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add("@FirstName", firstName);
+                p.Add("@LastName", lastName);
+
+                teachers = connection.ExecuteScalar<int>("dbo.spCheckTeacherByFirstLastName", p, commandType:CommandType.StoredProcedure);
+            }
+            return teachers != 0; 
+        }
+        public static LoggedUser GetLoginInfo(string login)
+        {
+            LoggedUser output = null; 
+
+            if (UserExists(login))
             {
                 using (SqlConnection connection = new SqlConnection(_dbConnectionStr))
                 {
                     DynamicParameters p = new DynamicParameters();
-                    p.Add("@Name",  userName);
+                    p.Add("@Login",  login);
 
-                    output = connection.Query<User>("dbo.spGetUserByName", p, commandType: CommandType.StoredProcedure).First();
+                    output = connection.Query<LoggedUser>("dbo.spGetLoginInfo", p, commandType: CommandType.StoredProcedure).First();
                 }
             }
 
             return output;
+        }
+        public static Teacher GetTeacherByLogin(string login)
+        {
+            Teacher output = null;
+            
+            if (UserExists(login))
+            {
+                using (SqlConnection connection = new SqlConnection(_dbConnectionStr))
+                {
+                    DynamicParameters p = new DynamicParameters();
+                    p.Add("@Login", login);
+                    
+                    //TODO test if there is no teacher
+                    output = connection.Query<Teacher>("dbo.GetTeacherByLogin", p, commandType: CommandType.StoredProcedure).First();
+                }
+            }
+
+            return output; 
         }
 
     }
